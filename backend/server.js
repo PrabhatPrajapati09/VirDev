2214283f86a01151bb7dc240316ae7078b650239
PrabhatPrajapati09 <prabhatprajapati0099@gmail.com>
2025-08-08 14:38:02 +0530

    starting with authentication

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
+const app = express();
+const port = process.env.PORT || 8800;
+
+connectDB();
+
+app.use(cors({credentials: true}));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
