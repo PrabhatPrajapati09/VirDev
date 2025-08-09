a6346443c6cf28382f26268e0c2b101fc4697fa8
PrabhatPrajapati09 <prabhatprajapati0099@gmail.com>
2025-08-09 15:01:19 +0530

    setting up routes for register and login apis and starting with mailing

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,11 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/userModel.js";
+import User from "../models/User.js";
+import transporter from "../config/nodemailer.js";
 
 
 export const register = async (req, res) => {
-    const {firstname, lastname, username, email, password } = req.body;
+    const { firstname, lastname, username, email, password } = req.body;
 
     if (!firstname || !lastname || !username || !email || !password) {
         return res.json({ success: false, message: 'All fields are required' });
@@ -17,7 +18,7 @@ export const register = async (req, res) => {
             return res.json({ success: false, message: 'User already exists' });
         }
 
-        hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         const user = new User({
             firstname,
@@ -29,7 +30,7 @@ export const register = async (req, res) => {
 
         await user.save();
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {expiresIn: '1d'});
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
         res.cookie('token', token, {
             httpOnly: true,
@@ -38,6 +39,16 @@ export const register = async (req, res) => {
             maxAge: 24 * 60 * 60 * 1000
         });
 
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: 'Welcome to VirDev',
+            text: `Hi ${firstname},\n\nWelcome to VirDev! Your account has been successfully created.\nVerify yourself by clicking the verify botton on the site.\n\nBest regards,\nVirDev Team`,
+        }
+
+        await transporter.sendMail(mailOptions);
+
+
         return res.json({ success: true, message: 'User registered successfully' });
     } catch (error) {
         res.json({ success: false, message: error.message });
@@ -45,7 +56,7 @@ export const register = async (req, res) => {
 }
 
 
-const login = async (req, res) => {
+export const login = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
@@ -53,6 +64,50 @@ const login = async (req, res) => {
     }
 
     try {
-        
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.json({ success: false, message: 'User not found' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.json({ success: false, message: 'Invalid password' });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 24 * 60 * 60 * 1000
+        });
+
+        return res.json({ success: true, message: 'User logged in successfully' });
+
+    }
+    catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+}
+
+
+export const logout = async (req, res) => {
+    try {
+
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        });
+
+        return res.json({ success: true, message: 'User logged out successfully' });
+    }
+    catch (error) {
+        return res.json({ success: false, message: error.message });
+
     }
 }
